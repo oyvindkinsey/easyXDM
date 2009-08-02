b66459d24e00005c68cc2ac577815ffbeafb6aa2
Sean Kinsey <oyvind@kinsey.no>
2009-08-02 15:45:24 +0200

    Fixed the build so that ant can handle documentation as well

--- package.cmd
+++
@@ -1,5 +0,0 @@
-call tools\apache-ant-1.7.1\bin\ant
-IF %ERRORLEVEL% == 0 java -jar tools\jsdoc_toolkit-2.3.0\jsdoc-toolkit\jsrun.jar tools\jsdoc_toolkit-2.3.0\jsdoc-toolkit\app\run.js -a -t=tools\jsdoc_toolkit-2.3.0\jsdoc-toolkit\templates/outline -x=js,html  -d=build\docs build\easyXSS.js build\hash.html
-IF %ERRORLEVEL% == 0 call tools\apache-ant-1.7.1\bin\ant package
-
-pause
