2de06d13e21659366a47ef1745ff1ddef7f22320
oyvind.kinsey <oyvind.kinsey@brik.no>
2010-02-27 19:39:17 +0100

    Fixed a typo

--- src/easyXDM.transport.HashTransport.js
+++ src/easyXDM.transport.HashTransport.js
@@ -8,7 +8,7 @@
      * HashTransport is a transport class that uses the IFrame URL Technique for communication.<br/>
      * <a href="http://msdn.microsoft.com/en-us/library/bb735305.aspx">http://msdn.microsoft.com/en-us/library/bb735305.aspx</a><br/>
      * This requires the precense of hash.html on the hosting domain for optimal performance, but can also be used with any other 
-     * static file present. If used with static file (like an image) as the local: property, you must set the readyAfter property.<br/>
+     * static file present. If used with a static file (like an image) as the local: property, you must set the readyAfter property.<br/>
      * The library will try to be ready earlier, so this should be set to a safe value, when you are certain that the file will be loaded.<br/>
      * If this file is already present in the document, and therefor most likely cached, then this can be set to low value.
      * <br/>
