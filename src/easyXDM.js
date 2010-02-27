543eddc1dd10f1035a04d3a49c6fac69137b2b30
oyvind.kinsey <oyvind.kinsey@brik.no>
2010-02-27 18:52:11 +0100

    Reverted and corrected the last commit

--- src/easyXDM.js
+++ src/easyXDM.js
@@ -139,23 +139,15 @@ easyXDM = {
                 var windowname = domain.replace(/\./g, "") + "easyxdm_log";
                 var logWin = window.open("", windowname, "width=800,height=200,status=0,navigation=0,scrollbars=1");
                 if (logWin) {
-                    var doc = logWin.document;
-                    if (doc.title !== "easyXDM log") {
-                        doc.write("<html><head><title>easyXDM log " + domain + "</title></head>");
-                        doc.write("<body><div id=\"log\"></div></body></html>");
-                        doc.close();
-                    }
-                    el = doc.getElementById("log");
-                    trace = function(msg){
+                    el = logWin.document.getElementById("log");
+                    clear = function(){
                         try {
-                            el.appendChild(doc.createElement("div")).appendChild(doc.createTextNode(location.host + "-" + new Date().valueOf() + ":" + msg));
-                            el.scrollTop = el.scrollHeight;
+                            el.innerHTML = "";
                         } 
                         catch (e) {
                             //In case we are unloading
                         }
                     };
-                    trace("---- new logger at " + location.href);
                 }
                 else {
                     clear = function(){
@@ -205,11 +197,13 @@ easyXDM = {
                  * Create log window
                  * @ignore
                  */
-                var logWin = window.open("", "easyxdm_log", "width=800,height=200,status=0,navigation=0,scrollbars=1");
+                var domain = location.host;
+                var windowname = domain.replace(/\./g, "") + "easyxdm_log";
+                var logWin = window.open("", windowname, "width=800,height=200,status=0,navigation=0,scrollbars=1");
                 if (logWin) {
                     var doc = logWin.document;
                     if (doc.title !== "easyXDM log") {
-                        doc.write("<html><head><title>easyXDM log</title></head>");
+                        doc.write("<html><head><title>easyXDM log " + domain + "</title></head>");
                         doc.write("<body><div id=\"log\"></div></body></html>");
                         doc.close();
                     }
@@ -224,10 +218,6 @@ easyXDM = {
                         }
                     };
                     trace("---- new logger at " + location.href);
-                }
-                else {
-                    trace = function(){
-                    };
                 }
             }
             else {
