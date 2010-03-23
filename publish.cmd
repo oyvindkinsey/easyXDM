32a77fbb7d4271c647e57952e7954affb3c0e7df
oyvind.kinsey <oyvind@kinsey.no>
2010-03-23 22:47:06 +0100

    Fixed a bug in the publish target

--- publish.cmd
+++ publish.cmd
@@ -1,4 +1,4 @@
-call tools\apache-ant-1.7.1\bin\ant publish -Dproject.build.artifactdir=pub/
+call tools\apache-ant-1.7.1\bin\ant publish -Dproject.build.publishdir=pub/
 IF NOT %ERRORLEVEL% == 0 pause
 
 
