2818ccd3b5447631485b8d3e1f7a860489acacc6
oyvind.kinsey <oyvind@kinsey.no>
2010-03-27 19:35:08 +0100

    Added the correct remote url to the sample

--- src/example/bookmark.js
+++ src/example/bookmark.js
@@ -1,12 +1,32 @@
 (function(){
     var s1, s2, isLoaded = false, xhr, head = document.getElementsByTagName('head')[0];
+    
+    var REMOTE = (function(){
+        var remote = location.href;
+        switch (location.host) {
+            case "provider.easyxdm.net":
+                location.href = remote.replace("provider", "consumer");
+                break;
+            case "easyxdm.net":
+                remote = remote.replace("easyxdm.net", "consumer.easyxdm.net");
+                break;
+            case "consumer.easyxdm.net":
+                remote = remote.replace("consumer", "provider");
+                break;
+            case "xdm1":
+                remote = remote.replace("xdm1", "xdm2");
+                break;
+        }
+        return remote.substring(0, remote.lastIndexOf("/"));
+    }());
+    
     function scriptOnLoad(){
         if (isLoaded || typeof easyXDM === "undefined" || typeof JSON === "undefined") {
             return;
         }
         isLoaded = true;
         xhr = new easyXDM.Rpc({
-            remote: "../xhr.html",
+            remote: REMOTE + "/../xhr.html",
             onReady: function(){
                 xhr.post("example/glossary.php", {
                     param1: "a",
