883f87578c755aa849e7e69ff53bf92df56d1d5f
oyvind.kinsey <oyvind@kinsey.no>
2010-03-27 18:43:24 +0100

    Completed the bookmark sample

--- src/example/bookmark.js
+++ src/example/bookmark.js
@@ -1,14 +1,14 @@
 (function(){
-    var script, isLoaded = false, xhr;
+    var s1, s2, isLoaded = false, xhr, head = document.getElementsByTagName('head')[0];
     function scriptOnLoad(){
-        if (isLoaded) {
+        if (isLoaded || typeof easyXDM === "undefined" || typeof JSON === "undefined") {
             return;
         }
         isLoaded = true;
         xhr = new easyXDM.Rpc({
             remote: "../xhr.html",
             onReady: function(){
-                xhr.post("example/glossary.json", {
+                xhr.post("example/glossary.php", {
                     param1: "a",
                     param2: "b"
                 }, function(json){
@@ -22,15 +22,28 @@
         });
         
     }
-    script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = "../easyXDM.debug.js";
-    script.onreadystatechange = function(){
+    s1 = document.createElement("script");
+    s1.type = "text/javascript";
+    s1.src = "../easyXDM.debug.js";
+    s1.onreadystatechange = function(){
         if (this.readyState === "complete" || this.readyState === "loaded") {
             scriptOnLoad();
         }
-    }
-    script.onload = scriptOnLoad;
+    };
+    s1.onload = scriptOnLoad;
+    head.appendChild(s1);
     
-    document.getElementsByTagName('head')[0].appendChild(script);
+    if (typeof JSON === "undefined" || !JSON) {
+        s2 = document.createElement("script");
+        s2.type = "text/javascript";
+        s2.src = "../json2.js";
+        s2.onreadystatechange = function(){
+            if (this.readyState === "complete" || this.readyState === "loaded") {
+                scriptOnLoad();
+            }
+        };
+        s2.onload = scriptOnLoad;
+        head.appendChild(s2);
+    }
+    
 })();
