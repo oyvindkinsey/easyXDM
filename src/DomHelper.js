oyvindkinsey/easyXDM

commit 91ed71795c8ca1d4a1743ac3ad4464ce2e49b5d9

Modified the code to avoid unnecessary property lookups and refactored common patterns

src/DomHelper.js
@@ -24,7 +24,7 @@ easyXDM.DomHelper = {
         var frame = document.createElement("IFRAME");
         frame.src = url;
         if (name) {
-            frame.id = frame.name = name;
+            frame.name = name;
         }
         if (onLoad) {
             frame.loadFn = function(){
@@ -36,7 +36,7 @@ easyXDM.DomHelper = {
             container.appendChild(frame);
         }
         else {
-			// This needs to be hidden like this, simply setting display:none and the like will cause failures in some browsers.
+            // This needs to be hidden like this, simply setting display:none and the like will cause failures in some browsers.
             frame.style.position = "absolute";
             frame.style.left = "-2000px";
             document.body.appendChild(frame);

build.xml
@@ -58,7 +58,6 @@
 		<!-- Process pre proccesing instructions like #if/#endif etc -->
 		<preprocess infile="work/easyXDM.combined.js" outfile="work/easyXDM.js"/>
 		<preprocess infile="work/easyXDM.combined.js" outfile="work/easyXDM.debug.js" defines="debug"/>
-		<preprocess infile="work/easyXDM.combined.js" outfile="work/easyXDM.contracts.js" defines="contracts"/>
 		<preprocess infile="${project.src.dir}/name.html" outfile="work/name.html"/>
 		<preprocess infile="${project.src.dir}/xhr.html" outfile="work/xhr.html"/>
 		<preprocess infile="${project.src.dir}/widgets/Widgets.js" outfile="work/easyXDM.Widgets.js"/>

src/Debug.js
@@ -1,5 +1,5 @@
 /*jslint evil: true, browser: true, immed: true, passfail: true, undef: true, newcap: true*/
-/*global console, _FirebugCommandLine,  easyXDM, window, escape, unescape, JSON */
+/*global console, _FirebugCommandLine,  easyXDM, window, escape, unescape, isHostObject, undef */
 
 // #ifdef debug
 /**
@@ -16,7 +16,7 @@ easyXDM.Debug = {
     log: function(msg){
         // Uses memoizing to cache the implementation
         var log;
-        if (typeof console === "undefined" || typeof console.log === "undefined") {
+        if (!isHostObject(window, "console") || undef(console.log)) {
             /**
              * Sets log to be an empty function since we have no output available
              * @ignore
@@ -59,7 +59,7 @@ easyXDM.Debug = {
                 }
             };
         }
-        else if (typeof console === "undefined" || typeof console.info === "undefined") {
+        else if (!isHostObject(window, "console") || undef(console.info)) {
             /**
              * Create log window
              * @ignore
@@ -121,7 +121,7 @@ easyXDM.Debug = {
                 }
             };
         }
-        else if (typeof console === "undefined" || typeof console.info === "undefined") {
+        else if (!isHostObject(window, "console") || undef(console.info)) {
             /**
              * Create log window
              * @ignore

src/Url.js
@@ -21,15 +21,14 @@ easyXDM.Url = {
         // #ifdef debug
         this._trace("parsing location.search: '" + location.search);
         // #endif
-        this._query = {};
-        var pair, key, value, search = location.search.substring(1).split("&");
-        for (var i = 0, len = search.length; i < len; i++) {
+        var query, pair, key, value, search = location.search.substring(1).split("&"), i = search.length;
+        while (i--) {
             pair = search[i];
             key = pair.substring(0, pair.indexOf("="));
             value = pair.substring(key.length + 1);
-            this._query[key] = value;
+            query[key] = value;
         }
-        return this._query;
+        return (this._query = query);
     },
     
     /**
@@ -46,11 +45,8 @@ easyXDM.Url = {
         // #endif
         var domain = url.substring(url.indexOf("//") + 2);
         domain = domain.substring(0, domain.indexOf("/"));
-        var _indexOf = domain.indexOf(":");
-        if (_indexOf != -1) {
-            domain = domain.substring(0, _indexOf);
-        }
-        return domain;
+        var indexOf = domain.indexOf(":");
+        return (indexOf === -1) ? domain : domain = domain.substring(0, indexOf);
     },
     
     /**
@@ -64,13 +60,8 @@ easyXDM.Url = {
             throw new Error("url is undefined or empty");
         }
         // #endif
-        var indexOf = url.indexOf("//");
-        var loc = url.substring(indexOf + 2);
-        if (loc.indexOf("/") == -1) {
-            return url;
-        }
-        loc = loc.substring(0, loc.indexOf("/"));
-        return url.substring(0, indexOf + 2) + loc;
+        var indexOf = url.indexOf("//"), loc = url.substring(indexOf + 2);
+        return (loc.indexOf("/") === -1) ? url : url.substring(0, indexOf + 2) + loc.substring(0, loc.indexOf("/"));
     },
     
     /**
@@ -125,13 +116,13 @@ easyXDM.Url = {
             throw new Error("parameters is undefined or null");
         }
         // #endif
-        var q = "";
+        var q = [];
         for (var key in parameters) {
             if (parameters.hasOwnProperty(key)) {
-                q += key + "=" + parameters[key] + "&";
+                q.push(key + "=" + parameters[key]);
             }
         }
-        return url + ((url.indexOf("?") == -1) ? "?" : "&") + q.substring(0, q.length - 1);
+        return url + ((url.indexOf("?") === -1) ? "?" : "&") + q.join("&");
     }
 };
 

src/easyXDM.js
@@ -13,6 +13,14 @@ function isHostObject(object, property){
     return !!(typeof(object[property]) == 'object' && object[property]);
 }
 
+function defer(fn, time){
+    return window.setTimeout(fn, time || 0);
+}
+
+function undef(v){
+    return typeof v === "undefined";
+}
+
 /** 
  * @class easyXDM
  * A javascript library providing cross-browser, cross-domain messaging/RPC.<br/>
@@ -53,7 +61,7 @@ easyXDM = {
      */
     prepareTransportStack: function(config){
         var query = easyXDM.Url.Query(), protocol = config.protocol, stackEls;
-        config.isHost = config.isHost || (typeof query.xdm_p === "undefined");
+        config.isHost = config.isHost || undef(query.xdm_p);
         // #ifdef debug
         this._trace("preparing transport stack");
         // #endif
@@ -65,7 +73,7 @@ easyXDM = {
             config.remote = decodeURIComponent(query.xdm_e);
             protocol = query.xdm_p;
         }
-        else if (typeof protocol === "undefined") {
+        else if (undef(protocol)) {
             config.remote = easyXDM.Url.resolveUrl(config.remote);
             if (isHostMethod(window, "postMessage")) {
                 protocol = "1";
@@ -142,11 +150,11 @@ easyXDM = {
                 else {
                     config.channel = query.xdm_c;
                     config.remote = decodeURIComponent(query.xdm_e);
-                    config.useParent = (typeof query.xdm_pa !== "undefined");
+                    config.useParent = !undef(query.xdm_pa);
                     if (config.useParent) {
                         config.useResize = false;
                     }
-                    config.usePolling = (typeof query.xdm_po !== "undefined");
+                    config.usePolling = !undef(query.xdm_po);
                 }
                 stackEls = [new easyXDM.stack.HashTransport(config), new easyXDM.stack.ReliableBehavior({
                     timeout: ((config.useResize ? 50 : config.interval * 1.5) + (config.usePolling ? config.interval * 1.5 : 50))

src/stack/HashTransport.js
@@ -1,5 +1,5 @@
 /*jslint evil: true, browser: true, immed: true, passfail: true, undef: true, newcap: true*/
-/*global easyXDM, window, escape, unescape */
+/*global easyXDM, window, escape, unescape, defer */
 
 /**
  * @class easyXDM.stack.HashTransport
@@ -58,7 +58,7 @@ easyXDM.stack.HashTransport = function(config){
         pub.up.incoming(_lastMsg.substring(_lastMsg.indexOf("_") + 1), _remoteOrigin);
     }
     
-    function _onResize(e){
+    function _onResize(){
         // #ifdef debug
         trace("onresize: new message");
         // #endif
@@ -154,7 +154,7 @@ easyXDM.stack.HashTransport = function(config){
                             return;
                         } 
                         catch (ex) {
-                            window.setTimeout(getRef, 50);
+                            defer(getRef, 50);
                         }
                     }());
                 }

src/stack/NameTransport.js
@@ -1,5 +1,5 @@
 /*jslint evil: true, browser: true, immed: true, passfail: true, undef: true, newcap: true*/
-/*global easyXDM, window, escape, unescape */
+/*global easyXDM, window, escape, unescape, defer */
 
 /**
  * @class easyXDM.stack.NameTransport
@@ -54,9 +54,9 @@ easyXDM.stack.NameTransport = function(config){
     
     function _onLoad(){
         if (callback) {
-            window.setTimeout(function(){
+            defer(function(){
                 callback(true);
-            }, 0);
+            });
         }
     }
     
@@ -77,10 +77,11 @@ easyXDM.stack.NameTransport = function(config){
             // #ifdef debug
             trace("init");
             // #endif
+            var dh = easyXDM.DomHelper, u = easyXDM.Url;
             isHost = config.isHost;
             readyCount = 0;
-            remoteOrigin = easyXDM.Url.getLocation(config.remote);
-            config.local = easyXDM.Url.resolveUrl(config.local);
+            remoteOrigin = u.getLocation(config.remote);
+            config.local = u.resolveUrl(config.local);
             
             if (isHost) {
                 // Register the callback
@@ -96,22 +97,22 @@ easyXDM.stack.NameTransport = function(config){
                 });
                 
                 // Set up the frame that points to the remote instance
-                remoteUrl = easyXDM.Url.appendQueryParameters(config.remote, {
+                remoteUrl = u.appendQueryParameters(config.remote, {
                     xdm_e: config.local,
                     xdm_c: config.channel,
                     xdm_p: 2
                 });
                 
-                remoteWindow = easyXDM.DomHelper.createFrame(remoteUrl + '#' + config.channel, config.container, null, config.channel);
+                remoteWindow = dh.createFrame(remoteUrl + '#' + config.channel, config.container, null, config.channel);
             }
             else {
                 config.remoteHelper = config.remote;
                 easyXDM.Fn.set(config.channel, _onMessage);
             }
             // Set up the iframe that will be used for the transport
-            callerWindow = easyXDM.DomHelper.createFrame(config.local + "#_4" + config.channel, null, function(){
+            callerWindow = dh.createFrame(config.local + "#_4" + config.channel, null, function(){
                 // Remove the handler
-                easyXDM.DomHelper.un(callerWindow, "load", callerWindow.loadFn);
+                dh.un(callerWindow, "load", callerWindow.loadFn);
                 easyXDM.Fn.set(config.channel + "_load", _onLoad);
                 _onReady();
             });

src/stack/PostMessageTransport.js
@@ -1,5 +1,5 @@
 /*jslint evil: true, browser: true, immed: true, passfail: true, undef: true, newcap: true*/
-/*global easyXDM, window, escape, unescape */
+/*global easyXDM, window, escape, unescape , defer*/
 
 /**
  * @class easyXDM.stack.PostMessageTransport
@@ -21,6 +21,7 @@ easyXDM.stack.PostMessageTransport = function(config){
  frame, // the remote frame, if any
  callerWindow, // the window that we will call with
  targetOrigin; // the domain to communicate with
+    var dh = easyXDM.DomHelper, u = easyXDM.Url;
     /**
      * Resolves the origin from the event object
      * @private
@@ -34,7 +35,7 @@ easyXDM.stack.PostMessageTransport = function(config){
         }
         if (event.uri) {
             // From earlier implementations 
-            return easyXDM.Url.getLocation(event.uri);
+            return u.getLocation(event.uri);
         }
         if (event.domain) {
             // This is the last option and will fail if the 
@@ -68,7 +69,7 @@ easyXDM.stack.PostMessageTransport = function(config){
             // #ifdef debug
             trace("destroy");
             // #endif
-            easyXDM.DomHelper.un(window, "message", _window_onMessage);
+            dh.un(window, "message", _window_onMessage);
             if (frame) {
                 callerWindow = null;
                 frame.parentNode.removeChild(frame);
@@ -79,24 +80,24 @@ easyXDM.stack.PostMessageTransport = function(config){
             // #ifdef debug
             trace("init");
             // #endif
-            targetOrigin = easyXDM.Url.getLocation(config.remote);
+            targetOrigin = u.getLocation(config.remote);
             if (config.isHost) {
                 // add the event handler for listening
-                easyXDM.DomHelper.on(window, "message", function waitForReady(event){
+                dh.on(window, "message", function waitForReady(event){
                     if (event.data == config.channel + "-ready") {
                         // #ifdef debug
                         trace("firing onReady");
                         // #endif
                         // replace the eventlistener
-                        easyXDM.DomHelper.un(window, "message", waitForReady);
-                        easyXDM.DomHelper.on(window, "message", _window_onMessage);
-                        window.setTimeout(function(){
+                        dh.un(window, "message", waitForReady);
+                        dh.on(window, "message", _window_onMessage);
+                        defer(function(){
                             pub.up.callback(true);
-                        }, 0);
+                        });
                     }
                 });
                 // set up the iframe
-                frame = easyXDM.DomHelper.createFrame(easyXDM.Url.appendQueryParameters(config.remote, {
+                frame = dh.createFrame(u.appendQueryParameters(config.remote, {
                     xdm_e: location.protocol + "//" + location.host,
                     xdm_c: config.channel,
                     xdm_p: 1 // 1 = PostMessage
@@ -106,12 +107,12 @@ easyXDM.stack.PostMessageTransport = function(config){
             }
             else {
                 // add the event handler for listening
-                easyXDM.DomHelper.on(window, "message", _window_onMessage);
+                dh.on(window, "message", _window_onMessage);
                 callerWindow = window.parent;
                 callerWindow.postMessage(config.channel + "-ready", targetOrigin);
-                window.setTimeout(function(){
+                defer(function(){
                     pub.up.callback(true);
-                }, 0);
+                });
             }
         }
     });

commit eb5264a83dfc27be10f7e711ccb0b39ab57a0d1f

Fixed a bug introduced with 91ed71795c8ca1d4a1743ac3ad4464ce2e49b5d9

src/DomHelper.js
@@ -24,7 +24,8 @@ easyXDM.DomHelper = {
         var frame = document.createElement("IFRAME");
         frame.src = url;
         if (name) {
-            frame.name = name;
+            //id needs to be set for the references to work reliably
+            frame.id = frame.name = name;
         }
         if (onLoad) {
             frame.loadFn = function(){