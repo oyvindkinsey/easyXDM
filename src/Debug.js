14d3ece4f6bb9e2da9ab49d23c06bbe42be279cf
oyvind.kinsey <oyvind@kinsey.no>
2010-07-27 13:49:49 +0200

    Improved the tracing facilities. This was needed as the whenReady feature ment that more code would be run prior to dom_onLoaded.

--- src/Debug.js
+++ src/Debug.js
@@ -1,9 +1,18 @@
 /*jslint evil: true, browser: true, immed: true, passfail: true, undef: true, newcap: true*/
-/*global console, _FirebugCommandLine,  easyXDM, window, escape, unescape, isHostObject, undef, _trace */
+/*global console, _FirebugCommandLine,  easyXDM, window, escape, unescape, isHostObject, undef, _trace, isReady, emptyFn */
 
 // #ifdef debug
 
 var debug = {
+    _deferred: [],
+    flush: function(){
+        this.trace("... deferred messages ...");
+        for (var i = 0, len = this._deferred.length; i < len; i++) {
+            this.trace(this._deferred[i]);
+        }
+        this._deferred.length = 0;
+        this.trace("... end of deferred messages ...");
+    },
     /**
      * Logs the message to console.log if available
      * @param {String} msg The message to log
@@ -15,8 +24,7 @@ var debug = {
              * Sets log to be an empty function since we have no output available
              * @ignore
              */
-            this.log = function(){
-            };
+            this.log = emptyFn;
         }
         else {
             /**
@@ -37,67 +45,81 @@ var debug = {
      */
     trace: function(msg){
         // Uses memoizing to cache the implementation
-        var el = document.getElementById("log");
-        if (el) {
-            /**
-             * Sets trace to be a function that outputs the messages to the DOMElement with id "log"
-             * @ignore
-             * @param {String} msg
-             */
-            this.trace = function(msg){
-                try {
-                    el.appendChild(document.createElement("div")).appendChild(document.createTextNode(location.host + "-" + new Date().valueOf() + ":" + msg));
-                    el.scrollTop = el.scrollHeight;
-                } 
-                catch (e) {
-                    //In case we are unloading
-                }
-            };
+        if (!isReady) {
+            if (this._deferred.length === 0) {
+                easyXDM.whenReady(debug.flush, debug);
+            }
+            this._deferred.push(msg);
+            this.log(msg);
         }
-        else if (!isHostObject(window, "console") || undef(console.info)) {
-            /**
-             * Create log window
-             * @ignore
-             */
-            var domain = location.host, windowname = domain.replace(/\.|:/g, "") + "easyxdm_log";
-            var logWin = window.open("", windowname, "width=800,height=200,status=0,navigation=0,scrollbars=1");
-            if (logWin) {
-                var doc = logWin.document;
-                el = doc.getElementById("log");
-                if (!el) {
-                    doc.write("<html><head><title>easyXDM log " + domain + "</title></head>");
-                    doc.write("<body><div id=\"log\"></div></body></html>");
-                    doc.close();
-                    el = doc.getElementById("log");
-                }
+        else {
+            var el = document.getElementById("log");
+            // is there a log element present?
+            if (el) {
+                /**
+                 * Sets trace to be a function that outputs the messages to the DOMElement with id "log"
+                 * @ignore
+                 * @param {String} msg
+                 */
                 this.trace = function(msg){
                     try {
-                        el.appendChild(doc.createElement("div")).appendChild(doc.createTextNode(location.host + "-" + new Date().valueOf() + ":" + msg));
+                        el.appendChild(document.createElement("div")).appendChild(document.createTextNode(location.host + "-" + new Date().valueOf() + ":" + msg));
                         el.scrollTop = el.scrollHeight;
                     } 
                     catch (e) {
                         //In case we are unloading
                     }
                 };
-                this.trace("---- new logger at " + location.href);
             }
-            else {
-                // We are unable to use any logging
-                this.trace = function(){
+            else if (isHostObject(window, "console") && !undef(console.info)) {
+                /**
+                 * Sets trace to be a wrapper around console.info
+                 * @ignore
+                 * @param {String} msg
+                 */
+                this.trace = function(msg){
+                    console.info(location.host + "-" + new Date().valueOf() + ":" + msg);
                 };
             }
+            else {
+                /**
+                 * Create log window
+                 * @ignore
+                 */
+                var domain = location.host, windowname = domain.replace(/\.|:/g, "") + "easyxdm_log", logWin;
+                try {
+                    logWin = window.open("", windowname, "width=800,height=200,status=0,navigation=0,scrollbars=1");
+                } 
+                catch (e) {
+                }
+                if (logWin) {
+                    var doc = logWin.document;
+                    el = doc.getElementById("log");
+                    if (!el) {
+                        doc.write("<html><head><title>easyXDM log " + domain + "</title></head>");
+                        doc.write("<body><div id=\"log\"></div></body></html>");
+                        doc.close();
+                        el = doc.getElementById("log");
+                    }
+                    this.trace = function(msg){
+                        try {
+                            el.appendChild(doc.createElement("div")).appendChild(doc.createTextNode(location.host + "-" + new Date().valueOf() + ":" + msg));
+                            el.scrollTop = el.scrollHeight;
+                        } 
+                        catch (e) {
+                            //In case we are unloading
+                        }
+                    };
+                    this.trace("---- new logger at " + location.href);
+                }
+                
+                if (!el) {
+                    // We are unable to use any logging
+                    this.trace = emptyFn;
+                }
+            }
+            this.trace(msg);
         }
-        else {
-            /**
-             * Sets trace to be a wrapper around console.info
-             * @ignore
-             * @param {String} msg
-             */
-            this.trace = function(msg){
-                console.info(location.host + "-" + new Date().valueOf() + ":" + msg);
-            };
-        }
-        this.trace(msg);
     },
     /**
      * Creates a method usable for tracing.
