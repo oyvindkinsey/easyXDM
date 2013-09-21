oyvindkinsey/easyXDM

commit c77cd045d5f5cc540b872c70ed0ad47c0eda58f5

url should be resolved in switch/case statement rather then detection in case config.protocol is manually specified

src/Core.js
@@ -671,7 +671,6 @@ function prepareTransportStack(config){
                  * navigating from one domain to another, and where parent.frames[foo] can be used
                  * to get access to a frame from the same domain
                  */
-                config.remoteHelper = resolveUrl(config.remoteHelper);
                 protocol = "2";
             }
             else {
@@ -769,6 +768,7 @@ function prepareTransportStack(config){
             stackEls = [new easyXDM.stack.PostMessageTransport(config)];
             break;
         case "2":
+            config.remoteHelper = resolveUrl(config.remoteHelper);
             stackEls = [new easyXDM.stack.NameTransport(config), new easyXDM.stack.QueueBehavior(), new easyXDM.stack.VerifyBehavior({
                 initiate: config.isHost
             })];

commit 19933fb24d32438bd383e9dca6077ac347615877

Fix bug in NameTransport introduced by c77cd045d5f5cc540b872c70ed0ad47c0eda58f5

src/Core.js
@@ -768,7 +768,9 @@ function prepareTransportStack(config){
             stackEls = [new easyXDM.stack.PostMessageTransport(config)];
             break;
         case "2":
-            config.remoteHelper = resolveUrl(config.remoteHelper);
+            if (config.isHost) {
+                config.remoteHelper = resolveUrl(config.remoteHelper);
+            }
             stackEls = [new easyXDM.stack.NameTransport(config), new easyXDM.stack.QueueBehavior(), new easyXDM.stack.VerifyBehavior({
                 initiate: config.isHost
             })];